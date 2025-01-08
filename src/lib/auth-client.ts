889bcdc1fd898ae17ad11276547475d65e24e19a
Bruno Alves <nito.ba.dev@gmail.com>
2025-01-08 18:02:49 -0300

    feat: setup base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,10 @@
 import { createAuthClient } from 'better-auth/react'
+
+const baseURL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
+
 export const authClient = createAuthClient({
   // Get this url dynamically from the server
-  baseURL: 'http://localhost:3000',
+  baseURL,
 })
